d7bac87039bd2f5e726641e2a2bd0ffb2b7e696b
2V01 <74354169+2V01@users.noreply.github.com>
2020-12-13 17:10:09 +0300

    Add files via upload

--- GitHub project/Program.cs
+++ GitHub project/Program.cs
@@ -7,6 +7,7 @@ namespace GitHub_project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Another word!");
         }
     }
 }
